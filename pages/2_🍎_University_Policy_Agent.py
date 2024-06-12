dca15375b0786e79f313d7962db650f519b768ae
rgoodman-sl <68026685+rgoodman-sl@users.noreply.github.com>
2024-06-12 10:36:02 +0000

    hh

--- pages/2_🍎_University_Policy_Agent.py
+++ pages/2_🍎_University_Policy_Agent.py
@@ -70,8 +70,8 @@ if prompt:
             result = response.json()
             # with st.chat_message("assistant"):
             #     st.markdown(result)
-            if 'choices' in result[0]:
-                response=result[0]['choices'][0]['message']['content'].replace("NEWLINE ", "**") + "**" + "\n\n"
+            if 'choices' in result:
+                response=result['choices'][0]['message']['content'].replace("NEWLINE ", "**") + "**" + "\n\n"
                 # Display assistant response in chat message container
                 with st.chat_message("assistant"):
                     typewriter(text=response, speed=10)
@@ -80,7 +80,7 @@ if prompt:
             else:
                 with st.chat_message("assistant"):
                     st.error(f"❌ Error in the SnapLogic API response")
-                    st.error(f"{result[0]['reason']}")
+                    st.error(f"{result['reason']}")
         else:
             with st.chat_message("assistant"):
                 st.error(f"❌ Error while calling the SnapLogic API")
