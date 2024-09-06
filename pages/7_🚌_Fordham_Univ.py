d54b49776815289653047d69b0f4bbea572583c9
snaplogic-snappy <167817032+snaplogic-snappy@users.noreply.github.com>
2024-09-06 12:35:17 +0200

    Update 7_🚌_Fordham_Univ.py
    Fixed chat history + adding Spinner

--- pages/7_🚌_Fordham_Univ.py
+++ pages/7_🚌_Fordham_Univ.py
@@ -24,11 +24,11 @@ st.markdown(
     """)
 
 # Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if "FU_messages" not in st.session_state:
+    st.session_state.FU_messages = []
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
+for message in st.session_state.FU_messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
@@ -37,32 +37,33 @@ prompt = st.chat_input("Ask me anything related to Fordham University Regulation
 if prompt:
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.FU_messages.append({"role": "user", "content": prompt})
 
-    URL = 'https://elastic.snaplogic.com/api/1/rest/slsched/feed/ConnectFasterInc/Demo_GenAI_App_Builder_NA/Fordham%20University/Fordham%20University%20-%20RAG%20Task'
-    BEARER_TOKEN = 'ndllpQvqupy08wYBS5ilDmYi8nmYz9FW'
-
-    data = {"prompt" : prompt}
-
-    headers = {
-        'Authorization': f'Bearer {BEARER_TOKEN}'
-    }
-
-    response = requests.post(
-        url=URL,
-        data=data,
-        headers=headers,
-        timeout=180,
-        verify=False
-    )
-
-    result = response.json()
-    #st.write(result)
-    response=result[0]['choices'][0]['message']['content']
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        #st.markdown(response,unsafe_allow_html=True)
-        typewriter(text=response, speed=10)
-
-# Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    with st.spinner("Working..."):
+        URL = 'https://elastic.snaplogic.com/api/1/rest/slsched/feed/ConnectFasterInc/Demo_GenAI_App_Builder_NA/Fordham%20University/Fordham%20University%20-%20RAG%20Task'
+        BEARER_TOKEN = 'ndllpQvqupy08wYBS5ilDmYi8nmYz9FW'
+    
+        data = {"prompt" : prompt}
+    
+        headers = {
+            'Authorization': f'Bearer {BEARER_TOKEN}'
+        }
+    
+        response = requests.post(
+            url=URL,
+            data=data,
+            headers=headers,
+            timeout=180,
+            verify=False
+        )
+    
+        result = response.json()
+        #st.write(result)
+        response=result[0]['choices'][0]['message']['content']
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            #st.markdown(response,unsafe_allow_html=True)
+            typewriter(text=response, speed=10)
+    
+    # Add assistant response to chat history
+        st.session_state.FU_messages.append({"role": "assistant", "content": response})
