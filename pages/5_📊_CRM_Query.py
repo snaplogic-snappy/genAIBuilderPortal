6217dfd36b6cf59f76b45d569741d0d94df09f28
snaplogic-snappy <167817032+snaplogic-snappy@users.noreply.github.com>
2024-09-06 12:29:31 +0200

    Update 5_📊_CRM_Query.py
    Fixed chat history + adding Spinner

--- pages/5_📊_CRM_Query.py
+++ pages/5_📊_CRM_Query.py
@@ -21,11 +21,11 @@ st.markdown(
  """)
 
 # Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if "CRM_messages" not in st.session_state:
+    st.session_state.CRM_messages = []
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
+for message in st.session_state.CRM_messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
@@ -34,32 +34,33 @@ prompt = st.chat_input("Ask me anything related to Salesforce Pipeline, Accounts
 if prompt:
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.CRM_messages.append({"role": "user", "content": prompt})
 
-    URL = 'https://snapgpt.labs.snaplogicdev.com/api/1/rest/slsched/feed/snaplogic/projects/Nilesh%20Parmar/GenAI_SOQL_Generation_NP_Task'
-    BEARER_TOKEN ='KKsuIzfnymYwRbzKuKsazMo1cE9bQ1QM'
-
-    data = {"prompt" : prompt}
-
-    headers = {
-        'Authorization': f'Bearer {BEARER_TOKEN}'
-    }
-
-    response = requests.post(
-        url=URL,
-        data=data,
-        headers=headers,
-        timeout=180,
-        verify=False
-    )
-
-    result = response.json()
-    #st.write(result)
-    response=result[0]['choices'][0]['message']['content']
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        #st.markdown(response,unsafe_allow_html=True)
-        typewriter(text=response, speed=10)
-
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    with st.spinner("Working..."):
+        URL = 'https://snapgpt.labs.snaplogicdev.com/api/1/rest/slsched/feed/snaplogic/projects/Nilesh%20Parmar/GenAI_SOQL_Generation_NP_Task'
+        BEARER_TOKEN ='KKsuIzfnymYwRbzKuKsazMo1cE9bQ1QM'
+    
+        data = {"prompt" : prompt}
+    
+        headers = {
+            'Authorization': f'Bearer {BEARER_TOKEN}'
+        }
+    
+        response = requests.post(
+            url=URL,
+            data=data,
+            headers=headers,
+            timeout=180,
+            verify=False
+        )
+    
+        result = response.json()
+        #st.write(result)
+        response=result[0]['choices'][0]['message']['content']
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            #st.markdown(response,unsafe_allow_html=True)
+            typewriter(text=response, speed=10)
+    
+        # Add assistant response to chat history
+        st.session_state.CRM_messages.append({"role": "assistant", "content": response})
