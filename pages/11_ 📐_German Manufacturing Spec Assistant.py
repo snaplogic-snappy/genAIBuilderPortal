461ff04cb63fd51d52ae0e616d8259e1189a7ecc
snaplogic-snappy <167817032+snaplogic-snappy@users.noreply.github.com>
2024-09-06 12:41:38 +0200

    Update 11_ 📐_German Manufacturing Spec Assistant.py
    Fixed chat history + adding Spinner

--- pages/11_ 📐_German Manufacturing Spec Assistant.py
+++ pages/11_ 📐_German Manufacturing Spec Assistant.py
@@ -64,12 +64,12 @@ if st.session_state.doc_status == 1:
     st.chatDisabled = False
 
 # Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if "GM_messages" not in st.session_state:
+    st.session_state.GM_messages = []
 
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
+for message in st.session_state.GM_messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
@@ -78,33 +78,34 @@ prompt = st.chat_input(placeholder=st.inputmsg,disabled=st.chatDisabled)
 if prompt:
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.GM_messages.append({"role": "user", "content": prompt})
 
-    URL = 'https://abd36121231c84034b34129e798681ad-485346289.eu-west-3.elb.amazonaws.com/api/1/rest/feed-master/queue/ConnectFasterInc/RG/msg/MSG_Ultra%20Task'
-    BEARER_TOKEN = 's7R0GSJF7AfAayGFrSxHeFC3Tc1tjo3P'
-
-    data = {"prompt" : prompt, "Document" : st.session_state.doc_key}
-
-    headers = {
-        'Authorization': f'Bearer {BEARER_TOKEN}'
-    }
+    with st.spinner("Working..."):
+        URL = 'https://abd36121231c84034b34129e798681ad-485346289.eu-west-3.elb.amazonaws.com/api/1/rest/feed-master/queue/ConnectFasterInc/RG/msg/MSG_Ultra%20Task'
+        BEARER_TOKEN = 's7R0GSJF7AfAayGFrSxHeFC3Tc1tjo3P'
     
-    response = requests.post(
-        url=URL,
-        data=data,
-        headers=headers,
-        timeout=180,
-        verify=False
-    )
-
-    result = response.json()
-    #st.write(result)
-    response=result['choices'][0]['message']['content']
-
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response,unsafe_allow_html=True)
-        #typewriter(text=response, speed=10)
-
-# Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        data = {"prompt" : prompt, "Document" : st.session_state.doc_key}
+    
+        headers = {
+            'Authorization': f'Bearer {BEARER_TOKEN}'
+        }
+        
+        response = requests.post(
+            url=URL,
+            data=data,
+            headers=headers,
+            timeout=180,
+            verify=False
+        )
+    
+        result = response.json()
+        #st.write(result)
+        response=result['choices'][0]['message']['content']
+    
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response,unsafe_allow_html=True)
+            #typewriter(text=response, speed=10)
+    
+    # Add assistant response to chat history
+        st.session_state.GM_messages.append({"role": "assistant", "content": response})
