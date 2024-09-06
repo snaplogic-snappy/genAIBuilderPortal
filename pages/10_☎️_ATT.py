bcc28956929bc35f137b6cbcf55e44c6876ca35e
snaplogic-snappy <167817032+snaplogic-snappy@users.noreply.github.com>
2024-09-06 12:39:21 +0200

    Update 10_☎️_ATT.py
    Fixed chat history + adding Spinner

--- pages/10_☎️_ATT.py
+++ pages/10_☎️_ATT.py
@@ -25,11 +25,11 @@ st.markdown(
  """)
 
 # Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if "ATT_messages" not in st.session_state:
+    st.session_state.ATT_messages = []
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
+for message in st.session_state.ATT_messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
@@ -38,32 +38,33 @@ prompt = st.chat_input("Ask me anything related to AT&T 5G Service and Protectio
 if prompt:
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.ATT_messages.append({"role": "user", "content": prompt})
 
-    URL = 'https://elastic.snaplogic.com/api/1/rest/slsched/feed/ConnectFasterInc/Demo_GenAI_App_Builder_NA/AT%26T/ACB%20-%20GenAI%20Step%202%20-%20ATT%20-%20RAG%20Task'
-    BEARER_TOKEN = 'kQAK3K0nrlQf7Qgit0TGbJOYC1Bb2nPS'
-
-    data = {"prompt" : prompt}
-
-    headers = {
-        'Authorization': f'Bearer {BEARER_TOKEN}'
-    }
-
-    response = requests.post(
-        url=URL,
-        data=data,
-        headers=headers,
-        timeout=180,
-        verify=False
-    )
-
-    result = response.json()
-    #st.write(result)
-    response=result[0]['choices'][0]['message']['content']
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        #st.markdown(response,unsafe_allow_html=True)
-        typewriter(text=response, speed=10)
-
-# Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    with st.spinner("Working..."):
+        URL = 'https://elastic.snaplogic.com/api/1/rest/slsched/feed/ConnectFasterInc/Demo_GenAI_App_Builder_NA/AT%26T/ACB%20-%20GenAI%20Step%202%20-%20ATT%20-%20RAG%20Task'
+        BEARER_TOKEN = 'kQAK3K0nrlQf7Qgit0TGbJOYC1Bb2nPS'
+    
+        data = {"prompt" : prompt}
+    
+        headers = {
+            'Authorization': f'Bearer {BEARER_TOKEN}'
+        }
+    
+        response = requests.post(
+            url=URL,
+            data=data,
+            headers=headers,
+            timeout=180,
+            verify=False
+        )
+    
+        result = response.json()
+        #st.write(result)
+        response=result[0]['choices'][0]['message']['content']
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            #st.markdown(response,unsafe_allow_html=True)
+            typewriter(text=response, speed=10)
+    
+    # Add assistant response to chat history
+        st.session_state.ATT_messages.append({"role": "assistant", "content": response})
