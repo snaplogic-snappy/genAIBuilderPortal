7d51d53514fed5c60d7e8b7455b023b70ca13e2f
Markus Pentzek <47973956+mpentzek@users.noreply.github.com>
2024-10-24 00:13:37 +0200

    Fixed button color and logo size

--- pages/15_🚀_SnapBot.py
+++ pages/15_🚀_SnapBot.py
@@ -34,7 +34,7 @@ st.markdown(
         font-size: 20px; 
         text-decoration: none; 
         color: white !important;  /* Force text color to white */
-        background-color: {primary_color}; 
+        background-color: #FF8831; 
         padding: 10px 20px; 
         border-radius: 5px;
     }}
@@ -42,7 +42,7 @@ st.markdown(
     <div class="centered">
         <h1>SnapBot - Your flexible Chatbot</h1>
         <p>SnapBot is a Retrieval-Augmented Generation (RAG) chatbot that allows you to upload PDF documents and ask specific questions about their content. It retrieves relevant information from one or multiple documents and generates dynamic, context-based responses derived from the text data.</p>
-        <img src="https://raw.githubusercontent.com/mpentzek/SnapBot/2fa63755e3bfda29e83163fb13577289f456b8e7/images/SnapBot-small-transbackg.png" alt="SnapBot Logo" width="300">
+        <img src="https://raw.githubusercontent.com/mpentzek/SnapBot/2fa63755e3bfda29e83163fb13577289f456b8e7/images/SnapBot-small-transbackg.png" alt="SnapBot Logo" width="200">
         <br>
         <h3>Some highlights:</h3>
         <ul>
