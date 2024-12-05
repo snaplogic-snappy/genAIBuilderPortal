1663bb55f3fa241ee67f74009848d9aba5609316
Toni <tbranco@snaplogic.com>
2024-12-05 20:37:22 -0300

    Replaced GenAI Builder refs to Agent Creator

--- GenAI_Demo.py
+++ GenAI_Demo.py
@@ -12,17 +12,17 @@ title=env["TITLE"]
 st.set_page_config(page_title=page_title)
 st.title(title)
 
-
+st.sidebar.title("Agent Creator Catalog")
 st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
     
-    ## SnapLogic GenAI App Builder allows you to create LLM-based applications in no time! 
+    ## SnapLogic Agent Creator allows you to create LLM-based applications in no time! 
     
-    ### **👈 Select a demo from the sidebar** to see some examples of what GenAI App Builder can do!
+    ### **👈 Select a demo from the sidebar** to see some examples of what Agent Creator can do!
     ## Want to learn more?
-    - Check out [GenAI Builder](https://www.snaplogic.com/products/genai-builder)
+    - Check out [Agent Creator](https://www.snaplogic.com/products/agent-creator)
     - Jump into our [documentation](https://docs-snaplogic.atlassian.net/wiki/spaces/SD/overview?homepageId=34537)
     - Ask a question in our [community](https://community.snaplogic.com)
 """
