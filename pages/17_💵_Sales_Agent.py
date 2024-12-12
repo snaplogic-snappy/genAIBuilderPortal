52e3887d30d2eba681d0a6e0ab7ba8646dda2621
gearilla <msager@yahoo.com>
2024-12-12 16:57:11 -0500

    Update 17_💵_Sales_Agent.py

--- pages/17_💵_Sales_Agent.py
+++ pages/17_💵_Sales_Agent.py
@@ -10,7 +10,7 @@ env = dotenv_values(".env")
 #URL = "https://emea.snaplogic.com/api/1/rest/slsched/feed/ConnectFasterInc/snapLogic4snapLogic/AutoRFPAgent/ApiRfpAgent"
 #BEARER_TOKEN = "nNpLBJrd8FAtFh3TVC9xR97QAwWtJHgF"
 #added by MS
-URL = "https://prodeu-connectfasterinc-cloud-fm.emea.snaplogic.io/gateway/Sales%20Agent/SalesAgent"
+URL = "https://prodeu-connectfasterinc-cloud-fm.emea.snaplogic.io/api/1/rest/feed/run/task/ConnectFasterInc/apim/SnapLogic%20Sales%20Agent/1.0/callAgentWorkerSalesAssistant"
 timeout = 180
 
 def typewriter(text: str, speed: int):
