cc1058c2fd2bed0b4fc3191a88c8f6d1b1ccc57f
gearilla <msager@yahoo.com>
2025-01-24 22:35:17 -0500

    Update Sales_Engineering_Management.py
    1st attempt to add a refresh button

--- pages/Sales_Engineering_Management.py
+++ pages/Sales_Engineering_Management.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
+
 st.set_page_config(layout="wide")
+
 def fetch_svg_from_api(url, token):
     """Fetch SVG content from API endpoint."""
     headers = {
@@ -22,16 +24,28 @@ def main():
     st.title("SE Management Dashboard (N.A.)")
     st.markdown('<p style="color:red;">Under Construction</p>', unsafe_allow_html=True)
     
-    
+    refresh_url = "https://elastic.snaplogic.com/api/1/rest/slsched/feed/ConnectFasterInc/Matt%20Sager%27s%20Project%20Space/SE%20Team%20Mgmt/AgentDriverSEActivities%20Task"
     api_url = "https://elastic.snaplogic.com/api/1/rest/slsched/feed/ConnectFasterInc/Matt%20Sager%27s%20Project%20Space/SE%20Team%20Mgmt/SEMonthlyActivitiesFetcher_Task"
-    bearer_token = "12345"  # Replace with actual token
+    bearer_token = "12345"
+    
     col1, col2 = st.columns([.6,.4])
+    
     with col1:
-        svg_content = fetch_svg_from_api(api_url, bearer_token)
+        # Initial load of SVG content
+        if 'svg_content' not in st.session_state:
+            st.session_state.svg_content = fetch_svg_from_api(api_url, bearer_token)
         
-        if svg_content:
+        if st.button('Refresh'):
+            with st.spinner('Refreshing data...'):
+                # First API call
+                refresh_response = requests.get(f"{refresh_url}?bearer_token={bearer_token}&reportType=monthly")
+                
+                # Second API call
+                st.session_state.svg_content = fetch_svg_from_api(api_url, bearer_token)
+        
+        if st.session_state.svg_content:
             import streamlit.components.v1 as components
-            components.html(svg_content, height=600, scrolling=True)
+            components.html(st.session_state.svg_content, height=600, scrolling=True)
 
 if __name__ == "__main__":
     main()
