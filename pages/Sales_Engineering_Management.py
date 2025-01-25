077f193ab50f2cc1b5d54ec974918e9c01a4b6f2
gearilla <msager@yahoo.com>
2025-01-25 12:52:00 -0500

    Update Sales_Engineering_Management.py

--- pages/Sales_Engineering_Management.py
+++ pages/Sales_Engineering_Management.py
@@ -21,8 +21,9 @@ def fetch_svg_from_api(url, token):
         return None
 
 def main():
-    <h3>st.title("SE Management Dashboard (N.A.)")</h3>
-        
+    st.title("SE Management Dashboard (N.A.)")
+    st.markdown("<h1 style='font-size: 24px;'>SE Management Dashboard (N.A.)</h1>", unsafe_allow_html=True)
+
     # Add a blinking subheading
     blinking_subheading = """
     <h4 style="color:red; animation: blink 1s step-start 3;">
