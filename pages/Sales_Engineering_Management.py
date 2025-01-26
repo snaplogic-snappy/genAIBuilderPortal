7b9e74b92f9220248c0ca7a41b8b7c3d5dcdcc0c
gearilla <msager@yahoo.com>
2025-01-26 11:27:19 -0500

    Update Sales_Engineering_Management.py

--- pages/Sales_Engineering_Management.py
+++ pages/Sales_Engineering_Management.py
@@ -69,11 +69,11 @@ def main():
         if 'svg_content_col2' not in st.session_state:
             st.session_state.svg_content_col2 = fetch_svg_from_api(api_url_with_params, bearer_token)
 
-            params = {'reportType': 'monthlyProductPercentages'}
-            api_url_with_params = f"{api_url}?{urllib.parse.urlencode(params)}"
+            #params = {'reportType': 'monthlyProductPercentages'}
+            #api_url_with_params = f"{api_url}?{urllib.parse.urlencode(params)}"
               # Initial load of SVG content
         #if 'svg_content_col2' not in st.session_state:
-            st.session_state.svg_content_col2 = fetch_svg_from_api(api_url_with_params, bearer_token)
+            #st.session_state.svg_content_col2 = fetch_svg_from_api(api_url_with_params, bearer_token)
         
         if st.button('Refreshx',disabled=True):
             with st.spinner('Refreshing data...'):
