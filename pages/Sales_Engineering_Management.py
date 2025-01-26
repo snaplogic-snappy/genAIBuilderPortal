344cc8e2f02e61ff76d14f2c07f0e6f8bcf24755
gearilla <msager@yahoo.com>
2025-01-26 12:56:45 -0500

    Update Sales_Engineering_Management.py

--- pages/Sales_Engineering_Management.py
+++ pages/Sales_Engineering_Management.py
@@ -83,12 +83,12 @@ def main():
             components.html(st.session_state.svg_content_col2, height=600, scrolling=True)
             
     params = {'reportType': 'monthlyProductPercentages'}
-        api_url_with_params = f"{api_url}?{urllib.parse.urlencode(params)}"
-              # Initial load of SVG content
-        if 'svg_content_bottom' not in st.session_state:
-            st.session_state.svg_content_bottom = fetch_svg_from_api(api_url_with_params, bearer_token)
-        if st.session_state.svg_content_bottom:
-            components.html(st.session_state.svg_content_bottom, height=600, scrolling=True)
+    api_url_with_params = f"{api_url}?{urllib.parse.urlencode(params)}"
+    # Initial load of SVG content
+    if 'svg_content_bottom' not in st.session_state:
+        st.session_state.svg_content_bottom = fetch_svg_from_api(api_url_with_params, bearer_token)
+    if st.session_state.svg_content_bottom:
+        components.html(st.session_state.svg_content_bottom, height=600, scrolling=True)
 
 
 if __name__ == "__main__":
