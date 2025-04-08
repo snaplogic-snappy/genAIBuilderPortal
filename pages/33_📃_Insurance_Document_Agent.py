a8d66d30d9c125744a2f28dc12dcc0baf1b79efa
bpatel-sl <141694040+bpatel-sl@users.noreply.github.com>
2025-04-07 22:33:34 -0500

    Update 33_📃_Insurance_Document_Agent.py

--- pages/33_📃_Insurance_Document_Agent.py
+++ pages/33_📃_Insurance_Document_Agent.py
@@ -10,8 +10,8 @@ URL = 'https://demo-fm.snaplogic.io/api/1/rest/feed/run/task/ConnectFasterInc/00
 BEARER_TOKEN = '123456'
 timeout = 90
 # Streamlit Page Properties
-page_title='Insurance Form/Document Agent'
-title='Insurance Form/Document Agent'
+page_title = 'Insurance Form/Document Agent'
+title = 'Insurance Form/Document Agent'
 
 def typewriter(text: str, speed: int):
     text = "Extracted Policy Information :," + text + ",-------------------------------------------------------------"
