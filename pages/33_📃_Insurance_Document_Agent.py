a3e66454b70caeec207a9b9e2eb9d9da6d687bf4
bpatel-sl <141694040+bpatel-sl@users.noreply.github.com>
2025-04-08 00:02:25 -0500

    Update 33_📃_Insurance_Document_Agent.py

--- pages/33_📃_Insurance_Document_Agent.py
+++ pages/33_📃_Insurance_Document_Agent.py
@@ -7,8 +7,8 @@ from dotenv import dotenv_values
 # Load environment
 env = dotenv_values(".env")
 # SnapLogic pipeline
-URL = "https://demo-fm.snaplogic.io/api/1/rest/feed/run/task/ConnectFasterInc/00_Bhavin%20Patel/GenAI/ExtractInsuranceDoc"
-BEARER_TOKEN = "123456"
+URL = 'https://demo-fm.snaplogic.io/api/1/rest/feed/run/task/ConnectFasterInc/00_Bhavin%20Patel/GenAI/ExtractInsuranceDoc'
+BEARER_TOKEN = '123456'
 timeout = 90
 # Streamlit Page Properties
 page_title = 'Insurance Form/Document Agent'
@@ -22,7 +22,6 @@ def typewriter(text: str, speed: int):
         #curr_full_text = " ".join(tokens[:index])
         container.markdown(text)
         time.sleep(1 / speed)
-
 st.set_page_config(page_title=page_title, layout="wide")
 st.title(title)
 
@@ -38,6 +37,7 @@ st.markdown(
     - Total Premium
     """
 )
+
 # Initialize chat history
 if "Binder_Extraction_messages" not in st.session_state:
     st.session_state.Binder_Extraction_messages = []
@@ -59,23 +59,4 @@ if uploaded_file is not None:
     time.sleep(0.5)
     if st.button(":blue[Extract!]"):
         with st.spinner("Parsing and Extracting Policy Info from PDF ..."):
-            
-            headers = {
-                'Authorization': f'Bearer {BEARER_TOKEN}',
-                'Content-Type': 'application/octet-stream'
-            }
 
-            response = requests.post(
-                url=URL,
-                data=file_bytes,
-                headers=headers,
-                timeout=180
-            )
-        print(response)
-        result = response.json()
-        response = result[0]['response']
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            typewriter(text=response, speed=10)
-        # Add assistant response to chat history
-        st.session_state.Binder_Extraction_messages.append({"role": "assistant", "content": response.replace("{", "").replace("}", "").replace(",", " ")})
