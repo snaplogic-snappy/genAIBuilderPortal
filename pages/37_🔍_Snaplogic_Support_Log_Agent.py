37871a7fb2f3ea0b4380196d34c74823eb63ec32
jhng-snaplogic <92174053+jhng-snaplogic@users.noreply.github.com>
2025-05-03 14:28:12 +0100

    Update 37_🔍_Snaplogic_Support_Log_Agent.py
    Improved format and configurations

--- pages/37_🔍_Snaplogic_Support_Log_Agent.py
+++ pages/37_🔍_Snaplogic_Support_Log_Agent.py
@@ -10,52 +10,75 @@ def typewriter(text: str, speed: int):
         container.markdown(curr_full_text)
         time.sleep(1 / speed)
 
+#Banner & Title
 st.set_page_config(
-    page_title="Support Agent Dashboard",
+    page_title="Support Log Agent",
     page_icon="🔍"
 )
 
 st.title("SnapLogic Support Agent (Preview)")
+
+st.subheader("This agent analyzes the log data of your environment and provides a detailed report for failed runtime executions.")
+st.markdown('')
+
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 # Create form
-
 with st.form("agent_form"):
-    st.write("Last Hours to Analyze")
-    slider_val = st.slider("Last Hours",min_value=1,max_value=1080,value=24,help="Select the number of past hours to analyse. The max is 1080 hours, equivalent to 45 days.")  #Last Hours slider
-    checkbox_val = st.checkbox("Get failed executions") #Get Execution Errors
-    checkbox_val = st.checkbox("Get successful executions") #Get Execution Errors
+    st.write("Last hours of runtime to assess")
+    last_hours = st.slider("Last Hours",min_value=1,max_value=1080,value=24,help="Select the number of past hours to analyze. The max is 1080 hours, equivalent to 45 days.")  #Last Hours slider
+    successful_exec = st.checkbox("Include successful executions") #Get Execution Errors
+
 
 # Submit button for the form
     submitted = st.form_submit_button(label="Submit",type="primary",icon=":material/check_circle:")
+    
 
 if submitted:
-    URL = 'https://prodeu-connectfasterinc-cloud-fm.emea.snaplogic.io/api/1/rest/feed-master/queue/ConnectFasterInc/snapLogic4snapLogic/SnapLogicSupportAgent/GetExecutionLogDataUltra'
-    BEARER_TOKEN ='5Mswx498pUh1B4rNVk3gUpLhiJmDYn5D'
+
+    progress_text = "Getting log analysis from the past "+str(last_hours)+" hours..."
+    my_bar = st.progress(0, text=progress_text)
+    for percent_complete in range(100):
+        time.sleep(0.2)
+        my_bar.progress(percent_complete + 1, text=progress_text)
+    time.sleep(1)
+
+    #pipeline url: https://cdn.emea.snaplogic.com/sl/designer.html#pipe_snode=6813b13d18cf2cce4fbcdaf5
+    URL = 'https://emea.snaplogic.com/api/1/rest/slsched/feed/ConnectFasterInc/snapLogic4snapLogic/SnapLogicSupportAgent/GetExecutionLogDataTask'
+    BEARER_TOKEN ='lgle9VfbVi5GE7VY8MkgOk2tkQLfrQ7e'
 
     headers = {
         'Authorization': f'Bearer {BEARER_TOKEN}'
+    }
+    
+    params = {
+    'hours': last_hours,
+    'inc_successful': str(successful_exec),
+    'state':"Failed"
     }
 
     response = requests.get(
         url=URL,
         headers=headers,
         timeout=180,
-        verify=False
+        verify=False,
+        params=params
     )
+
     if response.status_code == 200:
         result = response.json()
         post_content = result["response"]
                         
         # Display the generated post in a nice format
         with st.chat_message("assistant"):
+            my_bar.empty()
             st.markdown("### Extracted Execution Log Data")
             st.markdown("---")
             typewriter(text=post_content, speed=30)
-        
+
     
 
     #st.write(result)
